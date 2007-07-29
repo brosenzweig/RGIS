a56234cbc708a8b0ae027f022ac8e8cf542f0564
bmfekete <bmfekete@gmail.com>
2007-07-29 00:29:20 +0000

    Adding -g options to DBlib, RGlib and UIlib.

--- RGlib/common.mk
+++ RGlib/common.mk
@@ -3,14 +3,14 @@ UNIX=$(shell uname)
 ifeq ($(UNIX),Linux)
 export UNIXAR=ar -ru
 export UNIXCPP=g++
-export UNIXCPPOPS=-Wall -fsigned-char -D_GNU_SOURCE -Wno-deprecated
+export UNIXCPPOPS=-g -Wall -fsigned-char -D_GNU_SOURCE -Wno-deprecated
 export UNIXMAKE=make
 export UNIXRL=ranlib
 endif
 ifeq ($(UNIX),Darwin)
 export UNIXAR=ar -ru
 export UNIXCPP=g++
-export UNIXCPPOPS=-Wall -fsigned-char -D_GNU_SOURCE -Wno-deprecated
+export UNIXCPPOPS=-g -Wall -fsigned-char -D_GNU_SOURCE -Wno-deprecated
 export UNIXMAKE=make
 export UNIXRL=ranlib
 endif
