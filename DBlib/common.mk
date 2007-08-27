278b2e8fb4b99116e02781aa1009c6737a469868
bmfekete <bmfekete@gmail.com>
2007-08-27 19:43:49 +0000

    Fixing typo.

--- DBlib/common.mk
+++ DBlib/common.mk
@@ -3,7 +3,7 @@ UNIX=$(shell uname)
 ifeq ($(UNIX),Linux)
 export UNIXAR=ar -ru
 export UNIXCPP=g++
-export UNIXCPPOPS=-g -Wall -fsigned-char -D_GNU_SOURCE -Wno-deprecateds
+export UNIXCPPOPS=-g -Wall -fsigned-char -D_GNU_SOURCE -Wno-deprecated
 export UNIXMAKE=make
 export UNIXRL=ranlib
 endif
