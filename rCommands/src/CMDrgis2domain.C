5c7b48e4c0e146d8f2b351352e8b94ae5cf40ce7
bmfekete <bmfekete@gmail.com>
2007-11-27 17:26:38 +0000

    Adding longitude and latitude flags for non-geographic domains.

--- rCommands/src/CMDrgis2domain.C
+++ rCommands/src/CMDrgis2domain.C
@@ -22,13 +22,33 @@ int main (int argc,char *argv [])
 	FILE *outFile;
 	DBInt argPos, argNum = argc, ret;
 	int objID, size;
+	char *lonSTR = "LonDD";
+	char *latSTR = "LatDD";
 	MFDomain_t *domain = (MFDomain_t *) NULL;
 	DBCoordinate coord;
 	DBObjRecord *objRec;
 	DBObjData *data;
+	DBObjTableField *lonFLD = (DBObjTableField *) NULL;
+	DBObjTableField *latFLD = (DBObjTableField *) NULL;
 
 	for (argPos = 1;argPos < argNum; )
 		{
+		if (CMargTest (argv [argPos],"-n","--longitude"))
+			{
+			if ((argNum = CMargShiftLeft (argPos,argv,argNum)) <= argPos)
+				{ CMmsgPrint (CMmsgUsrError,"Missing symbol field!\n"); return (CMfailed); }
+			lonSTR = argv [argPos];
+			if ((argNum = CMargShiftLeft (argPos,argv,argNum)) <= argPos) break;
+			continue;
+			}
+		if (CMargTest (argv [argPos],"-a","--latitude"))
+			{
+			if ((argNum = CMargShiftLeft (argPos,argv,argNum)) <= argPos)
+				{ CMmsgPrint (CMmsgUsrError,"Missing symbol field!\n"); return (CMfailed); }
+			latSTR = argv [argPos];
+			if ((argNum = CMargShiftLeft (argPos,argv,argNum)) <= argPos) break;
+			continue;
+			}
 		if (CMargTest (argv [argPos],"-h","--help"))
 			{
 			CMmsgPrint (CMmsgInfo,"%s [options] <input rgisdata> <output domain>\n",CMprgName(argv[0]));
@@ -57,10 +77,26 @@ int main (int argc,char *argv [])
 			case DBTypeVectorPoint:
 				{
 				DBVPointIO *pntIO = new DBVPointIO (data);
+				DBObjTable *objTable = data->Table (DBrNItems);
+
+				lonFLD = objTable->Field (lonSTR);
+				if ((strcmp (lonSTR,"LonDD") != 0) && (lonFLD == (DBObjTableField *) NULL)) {
+					CMmsgPrint (CMmsgUsrError,"Invalid longitude field [%s}!",lonSTR);
+					delete pntIO;
+					delete data;
+					goto Stop;
+				}
+				latFLD = objTable->Field (latSTR);
+				if ((strcmp (latSTR,"LatDD") != 0) && (latFLD == (DBObjTableField *) NULL)) {						
+					CMmsgPrint (CMmsgUsrError,"Invalid latitude field [%s}!",latSTR);
+					delete pntIO;
+					delete data;
+					goto Stop;
+				}
 
 				domain->ObjNum = pntIO->ItemNum ();
 				if ((domain->Objects = (MFObject_t *) calloc (domain->ObjNum,sizeof (MFObject_t))) == (MFObject_t *) NULL)
-					{ perror ("Memory Allocation Error"); MFDomainFree (domain); goto Stop; }
+					{ perror ("Memory Allocation Error"); delete pntIO; delete data; MFDomainFree (domain); goto Stop; }
 				for (objID = 0;objID < domain->ObjNum;++objID)
 					{
 					objRec = pntIO->Item (objID);
@@ -70,11 +106,15 @@ int main (int argc,char *argv [])
 					domain->Objects [objID].ULinkNum = 0;
 					domain->Objects [objID].DLinks = (size_t *) NULL;
 					domain->Objects [objID].ULinks = (size_t *) NULL;
-					domain->Objects [objID].XCoord = domain->Objects [objID].Lon = coord.X;
-					domain->Objects [objID].YCoord = domain->Objects [objID].Lat = coord.Y;
+					domain->Objects [objID].XCoord = coord.X;
+					domain->Objects [objID].YCoord = coord.Y;
+					domain->Objects [objID].Lon = lonFLD != (DBObjTableField *) NULL ? lonFLD->Float (objRec) : coord.X;
+					domain->Objects [objID].Lat = latFLD != (DBObjTableField *) NULL ? latFLD->Float (objRec) : coord.Y;
+					
 					domain->Objects [objID].Area   = 0.0;
 					domain->Objects [objID].Length = 0.0;
 					}
+				delete pntIO;
 				} break;
 			case DBTypeGridContinuous:
 			case DBTypeGridDiscrete:
@@ -85,9 +125,26 @@ int main (int argc,char *argv [])
 				DBInt dir;
 				DBObjRecord *nextCell;
 				DBNetworkIO *netIO = new DBNetworkIO (data);
+				DBObjTable *objTable = data->Table (DBrNCells);
+
+				lonFLD = objTable->Field (lonSTR);
+				if ((strcmp (lonSTR,"LonDD") != 0) && (lonFLD == (DBObjTableField *) NULL)) {
+					CMmsgPrint (CMmsgUsrError,"Invalid longitude field [%s}!",lonSTR);
+					delete netIO;
+					delete data;
+					goto Stop;
+				}
+				latFLD = objTable->Field (latSTR);
+				if ((strcmp (latSTR,"LatDD") != 0) && (latFLD == (DBObjTableField *) NULL)) {						
+					CMmsgPrint (CMmsgUsrError,"Invalid latitude field [%s}!",latSTR);
+					delete netIO;
+					delete data;
+					goto Stop;
+				}
+
 				domain->ObjNum = netIO->CellNum ();
 				if ((domain->Objects = (MFObject_t *) calloc (domain->ObjNum,sizeof (MFObject_t))) == (MFObject_t *) NULL)
-					{ perror ("Memory Allocation Error"); MFDomainFree (domain); goto Stop; }
+					{ perror ("Memory Allocation Error"); delete netIO; delete data; MFDomainFree (domain); goto Stop; }
 				for (objID = 0;objID < domain->ObjNum;++objID)
 					{
 					domain->Objects [objID].DLinks = (size_t *) NULL;
@@ -100,8 +157,10 @@ int main (int argc,char *argv [])
 					domain->Objects [objID].ID = objRec->RowID ();
 					domain->Objects [objID].DLinkNum = 0;
 					domain->Objects [objID].ULinkNum = 0;
-					domain->Objects [objID].XCoord = domain->Objects [objID].Lon = coord.X;
-					domain->Objects [objID].YCoord = domain->Objects [objID].Lat = coord.Y;
+					domain->Objects [objID].XCoord = coord.X;
+					domain->Objects [objID].YCoord = coord.Y;
+					domain->Objects [objID].Lon = lonFLD != (DBObjTableField *) NULL ? lonFLD->Float (objRec) : coord.X;
+					domain->Objects [objID].Lat = latFLD != (DBObjTableField *) NULL ? latFLD->Float (objRec) : coord.Y;
 					domain->Objects [objID].Area   = netIO->CellArea   (objRec);
 					domain->Objects [objID].Length = netIO->CellLength (objRec);
 					if ((nextCell = netIO->ToCell (objRec)) != (DBObjRecord *) NULL)
@@ -122,9 +181,11 @@ int main (int argc,char *argv [])
 							domain->Objects [objID].ULinkNum++;
 							}
 					}
+				delete netIO;
 				} break;
 			}
 		ret = MFDomainWrite (domain,outFile);
+		delete data;
 		}
 Stop:
 	if (outFile != stdout) fclose (outFile);
