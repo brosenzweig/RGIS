21b134a1c3488a861652e81ec90cecdae3ce7505
bmfekete <bmfekete@gmail.com>
2009-10-03 00:36:04 +0000

    Fixing Makefile for pthread.

--- MFlib/common.mk
+++ MFlib/common.mk
@@ -3,21 +3,21 @@ UNIX=$(shell uname)
 ifeq ($(UNIX),Linux)
 export UNIXAR=ar -ru
 export UNIXCC=gcc
-export UNIXCCOPS=-g -Wall -fsigned-char -D_GNU_SOURCE
+export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
 export UNIXMAKE=make
 export UNIXRL=ranlib
 endif
 ifeq ($(UNIX),Darwin)
 export UNIXAR=ar -ru
 export UNIXCC=gcc
-export UNIXCCOPS=-g -Wall -fsigned-char -D_GNU_SOURCE
+export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
 export UNIXMAKE=make
 export UNIXRL=ranlib
 endif
 ifeq ($(UNIX),SunOS)
 export UNIXAR=ar -ru
 export UNIXCC=gcc
-export UNIXCCOPS=-g -Wall -fsigned-char -D_GNU_SOURCE
-export UNIXMAKE=make
+export UNIXCCOPS=-g -Wall -pthread -fsigned-char -D_GNU_SOURCE
+export UNIXMAKE=gmake
 export UNIXRL=ranlib
 endif
